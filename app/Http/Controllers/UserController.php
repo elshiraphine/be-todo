76d1edc96cd7084ed2df5fb902381a99c95bbfd4
elshiraphine <erviana.205025@mhs.its.ac.id>
2024-02-17 18:33:52 +0700

    feat(register): add registered email validation response and modify response structure

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Application\Services\UserServices\UserRegistrationService;
+use App\Core\Entity\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -25,14 +27,26 @@ class UserController extends Controller
 
     public function register(Request $request): \Illuminate\Http\JsonResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+        } catch (ValidationException $e) {
+            if ($e->errors()['email'][0] === 'The email has already been taken.') {
+                return apiResponse(409, 'error', ['message' => 'email_already_exists']);
+            } else {
+                return apiResponse(403, 'error', [
+                    'message' => 'invalid_input',
+                    'error' => $e->validator->errors()->first()
+                ]);
+            }
+        }
 
         $user = $this->userRegistrationService->register($request->only('name', 'email', 'password'));
 
-        return apiResponse(200, 'success', $user);
+        return apiResponse(200, 'success', ['message' => 'user_registered_successfully', 'user' => $user]);
     }
+
 }
